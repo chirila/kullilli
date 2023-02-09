8f95a93967b531268c34e5dd32575712d0ad7b19
sunmuffin42 <sunkulp.ananth@gmail.com>
2023-02-09 14:13:41 -0500

    add title to flibl jsons

--- kullilli-conv/json_construction_from_flextext.py
+++ kullilli-conv/json_construction_from_flextext.py
@@ -3,25 +3,31 @@ from xml.etree import ElementTree as ET
 import json
 
 # this is flibl for a no-bells-nor-whistles flextext: no time alignment, one vernacular and one analysis language, no speaker specification. all it does is make the fibl interchange json with glosses
-# text = "../001-064 Kullilli sentences.flextext" # for now
+
 gll_texts = [
-    "../001-064 Kullilli sentences.flextext",
-    "../065-128 Kullilli sentences.flextext",
-    "../129-192 Kullilli sentences.flextext",
-    "../193-256 Kullilli sentences.flextext",
-    "../257-320 Kullilli sentences.flextext",
-    "../321-384 Kullilli sentences.flextext",
-    "../385-448 Kullilli sentences.flextext",
-    "../449-514 Kullilli sentences.flextext",
-    "../Animal trouble in the camp.flextext",
-    "../Breen Sentences.flextext"
-    ] # etc
-# for text in gll_texts: etc
+    "../data/flextext/001-064 Kullilli sentences.flextext",
+    "../data/flextext/065-128 Kullilli sentences.flextext",
+    "../data/flextext/129-192 Kullilli sentences.flextext",
+    "../data/flextext/193-256 Kullilli sentences.flextext",
+    "../data/flextext/257-320 Kullilli sentences.flextext",
+    "../data/flextext/321-384 Kullilli sentences.flextext",
+    "../data/flextext/385-448 Kullilli sentences.flextext",
+    "../data/flextext/449-514 Kullilli sentences.flextext",
+    "../data/flextext/Animal trouble in the camp.flextext",
+    "../data/flextext/Breen Sentences.flextext"
+    ]
+
 for text in gll_texts:
     ft = ET.parse(text).getroot()
     new_json = {
         "flextext":text
     }
+    try:
+        for lang_title in ft.findall(".//item[@type='title']"):
+            new_json["title-{}".format(lang_title.attrib["lang"])] = lang_title.text
+    except:
+        pass
+
     morph_keys = ["txt", "cf", "gls", "msa", "variantTypes", "hn", "glsAppend" "morph_type"]
     for phrase in ft.findall(".//phrase"):
         segnum = phrase.find("./item[@type='segnum']").text
@@ -52,10 +58,12 @@ for text in gll_texts:
                 morph_list.append(morph_dict)
 
             word_dict["morphs"] = morph_list
+            # add word level part of speech
             try:
                 word_dict["pos"] = word.find("./item[@type='pos']").text
             except:
                 word_dict["pos"] = ""
+            # add word level gloss
             try:
                 word_dict["gls"] = word.find("./item[@type='gls']").text
             except:
@@ -63,14 +71,20 @@ for text in gll_texts:
 
             # put the word_dict in the word_list
             word_list.append(word_dict)
-        segdict["word_list"] = word_list
+        # add the phrase level baseline to the segdict
+        segdict["full_text"] = full_text
+        
         # translations = phrase.findall("./items[@type='gls']") # if we want to add a way to do this for flextexts with multiple analysis languages, find all the translation elements and make the distinct translation keys as you loop through them, appending -{}".format(translation.attrib["lang"]) to them
         # add translation to segdict
         try:
-            segdict["translation"] = phrase.find("./item[@type='gls']").text
+            for lang_translation in phrase.findall(".//item[@type='gls']"):
+                segdict["translation-{}".format(lang_translation.attrib["lang"])] = lang_translation.text
+            # segdict["translation"] = phrase.find("./item[@type='gls']").text
+
         except:
             segdict["translation"] = ""
-
+        # add the word list to the segdict
+        segdict["word_list"] = word_list
         # put notes into the segdict
         notes = []
         try:
@@ -79,12 +93,11 @@ for text in gll_texts:
         except:
             pass
         segdict["notes"] = notes
-        # add the phrase level baseline to the segdict
-        segdict["full_text"] = full_text
+        
 
 
         # add the segdict to the main dict
         new_json[segnum] = segdict
 
-
-    json.dump(new_json, open(text[:-8] + "json", mode="w", encoding="utf8"), indent=1)
+    # create the json
+    json.dump(new_json, open("../data/json/flibl-json" + text[16:-8] + "json", mode="w", encoding="utf8"), indent=1)
